679a8c9072f5ac7f4fc5f826f8339543b9b9b3f9
admazzola <admazzola@gmail.com>
2014-12-22 15:01:27 -0500

    Updated tables for parts

--- app/controllers/partdetail_controller.rb
+++ app/controllers/partdetail_controller.rb
@@ -1,5 +1,11 @@
 class PartdetailController < ApplicationController
   
+    def destroy
+    
+   Partdetail.find(params['id']).destroy
+    
+    redirect_to '/partdetail/', notice: 'deleted part'
+  end
   
   def partinfo
     code = params['id'];
@@ -15,7 +21,31 @@ class PartdetailController < ApplicationController
   end
   
   
-  
+  def create
+    # , "newpart"=>{"barcode"=>"grdgr", "catalognumber"=>"drgrdg", "description"=>"drgr"}, "action"=>"create", "controller"=>"partdetail"}
+    
+   
+        Productlibrary.all.each do |product|
+      if(product.catalog_number)
+      product.catalog_number = product.catalog_number.gsub(/[^0-9a-z ]/i, '').upcase
+      product.save
+      end
+      
+    end
+    
+    strippedcatalognumber = params['newpart']['catalognumber'].gsub(/[^0-9a-z ]/i, '')
+    
+    product = Productlibrary.where(:catalog_number => strippedcatalognumber).first
+    if(product == nil)
+      product = Productlibrary.new(:catalog_number => strippedcatalognumber,:description => params['newpart']['description'])
+      product.save
+     end
+    
+    part = Partdetail.new(:catalog_id => product.id,:barcode => params['newpart']['barcode'],:series => params['newpart']['series'])
+    part.save
+    
+    redirect_to '/partdetail/'
+  end
   
   
   
--- app/controllers/productlibrary_controller.rb
+++ app/controllers/productlibrary_controller.rb
@@ -1,2 +1,18 @@
 class ProductlibraryController < ApplicationController
+  
+  
+  def destroy
+    
+   Productlibrary.find(params['id']).destroy
+    
+    redirect_to '/productlibrary/', notice: 'deleted product'
+  end
+  
+  
+  
+  
+  
+  
+  
+  
 end
