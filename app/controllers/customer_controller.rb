6d78a9b6302b6ab965869ebd6837a209c6faaff2
admazzola <admazzola@gmail.com>
2014-12-22 15:36:22 -0500

    added customer deleting

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,2 +1,13 @@
 class CustomerController < ApplicationController
+  
+  
+   def destroy
+    
+   Customer.find(params['id']).destroy
+    
+    redirect_to '/customer/', notice: 'deleted customer'
+  end
+  
+  
+  
 end
